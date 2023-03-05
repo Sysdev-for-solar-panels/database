61a47bbd4c8fc0dbfdf51789434f01bae609781d
green1490 <46134377+green1490@users.noreply.github.com>
2023-03-05 13:36:12 +0000

    change into pgsql

--- data.sql
+++ data.sql
@@ -1,128 +1,52 @@
--- --------------------------------------------------------
--- Hoszt:                        127.0.0.1
--- Szerver verzió:               8.0.30 - MySQL Community Server - GPL
--- Szerver OS:                   Win64
--- HeidiSQL Verzió:              12.1.0.6537
--- --------------------------------------------------------
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(50) NOT NULL,
+  role VARCHAR(50) NOT NULL
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE TABLE projects (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  start_date DATE NOT NULL,
+  end_date DATE,
+  status VARCHAR(20) NOT NULL DEFAULT 'NEW',
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
 
+CREATE TABLE project_parts (
+  id SERIAL PRIMARY KEY,
+  project_id INTEGER NOT NULL REFERENCES projects(id),
+  part_name VARCHAR(50) NOT NULL,
+  quantity INTEGER NOT NULL,
+  storage_location VARCHAR(20) NOT NULL,
+  CONSTRAINT unique_part_location UNIQUE (project_id, storage_location)
+);
 
--- Adatbázis struktúra mentése a data.
-CREATE DATABASE IF NOT EXISTS `data` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `data`;
+CREATE TABLE project_logs (
+  id SERIAL PRIMARY KEY,
+  project_id INTEGER NOT NULL REFERENCES projects(id),
+  status VARCHAR(20) NOT NULL,
+  log_date DATE NOT NULL,
+  description VARCHAR(255),
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
 
--- Struktúra mentése tábla data. component
-CREATE TABLE IF NOT EXISTS `component` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `product_id` int NOT NULL,
-  `rack_id` int NOT NULL,
-  `quantity` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `product_id` (`product_id`),
-  KEY `rack_id` (`rack_id`),
-  CONSTRAINT `component_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
-  CONSTRAINT `component_ibfk_2` FOREIGN KEY (`rack_id`) REFERENCES `rack` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  order_date DATE NOT NULL,
+  part_name VARCHAR(50) NOT NULL,
+  quantity INTEGER NOT NULL,
+  supplier VARCHAR(50) NOT NULL
+);
 
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla data. order
-CREATE TABLE IF NOT EXISTS `order` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `customer_name` varchar(255) NOT NULL,
-  `customer_email` varchar(255) NOT NULL,
-  `total_price` decimal(10,2) NOT NULL,
-  `status` enum('New','Draft','Wait','Scheduled','InProgress','Completed','Failed') CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla data. order_item
-CREATE TABLE IF NOT EXISTS `order_item` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `order_id` int NOT NULL,
-  `product_id` int NOT NULL,
-  `quantity` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `order_id` (`order_id`),
-  KEY `product_id` (`product_id`),
-  CONSTRAINT `order_item_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`),
-  CONSTRAINT `order_item_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla data. product
-CREATE TABLE IF NOT EXISTS `product` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `description` text,
-  `price` decimal(10,2) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla data. projects
-CREATE TABLE IF NOT EXISTS `projects` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `customer_name` varchar(255) NOT NULL,
-  `start_date` date NOT NULL,
-  `end_date` date DEFAULT NULL,
-  `status` varchar(50) NOT NULL DEFAULT 'New',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla data. rack
-CREATE TABLE IF NOT EXISTS `rack` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `warehouse_id` int NOT NULL,
-  `row` int NOT NULL,
-  `column` int NOT NULL,
-  `level` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `warehouse_id` (`warehouse_id`),
-  CONSTRAINT `rack_ibfk_1` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouse` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla data. status_logs
-CREATE TABLE IF NOT EXISTS `status_logs` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `project_id` int NOT NULL,
-  `status` varchar(50) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- Az adatok exportálása nem lett kiválasztva.
-
--- Struktúra mentése tábla data. warehouse
-CREATE TABLE IF NOT EXISTS `warehouse` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `location` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- Az adatok exportálása nem lett kiválasztva.
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+CREATE TABLE inventory (
+  id SERIAL PRIMARY KEY,
+  part_name VARCHAR(50) NOT NULL,
+  quantity INTEGER NOT NULL,
+  storage_location VARCHAR(20) NOT NULL,
+  CONSTRAINT unique_location UNIQUE (part_name, storage_location)
+);
